7928c43f66851d630fb2ee79842e23ad1dffa92d
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-05 16:29:20 +0300

    added an .env file

--- config.py
+++ config.py
@@ -6,7 +6,10 @@ class Settings(BaseSettings):
     base_url: str = "http://localhost:8000"
     db_url : str = "sqlite:///./shortener.db"
 
-@lru_cache
+    class Config:
+        env_file= ".env"
+
+# @lru_cache
 def get_settings() -> Settings:
     settings = Settings()
     print(f"Loading settings for : {settings.env_name}")
