1f3de8ba11bb740fff6947a546b9f03d7295d39e
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-05 16:40:48 +0300

    defined the URL Base class

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     env_name: str = "Local"
     base_url: str = "http://localhost:8000"
-    db_url : str = "sqlite:///./shortener.db"
+    db_url : str = ""
 
     class Config:
         env_file= ".env"
