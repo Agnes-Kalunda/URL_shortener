e52f9330f1f75fba46b963d4af89d217d31cdf45
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-05 19:29:48 +0300

    created a create_url function

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import validators
+
+import schemas
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return ("URL SHORTENER!!")
+
+
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail =message)
+
+
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+
+    return f"TODO : Create database entry for : {url.target_url}"
